169b2fde174c66de9485a81f17f1803df6889de5
Sun Kai <l>
2020-07-03 15:44:33 +0800

    模型支持LSTM类型

--- network.py
+++ network.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import numpy as np
+from enum import Enum
 
 class Settings(object):
     def __init__(self):
@@ -11,7 +12,7 @@ class Settings(object):
         # 关系种类的总数
         self.num_classes = 5
         # 隐藏层的神经元数量
-        self.gru_size = 230
+        self.hidden_unit = 230
         self.keep_prob = 0.5
         # 全连接层的层数
         self.num_layers = 1
@@ -24,77 +25,77 @@ class Settings(object):
         self.lr = 0.0005
         # 正则项
         self.regularizer = 0.0001
+        # RNN类型
+        self.cell_type = RNN__CELL_TYPE.GRU
 
 
-class GRU:
+class RNN_MODEL:
     def __init__(self, is_training, word_embeddings, settings, session):
 
-        self.num_steps = num_steps = settings.num_steps
-        self.vocab_size = vocab_size = settings.vocab_size
-        self.num_classes = num_classes = settings.num_classes
-        self.gru_size = gru_size = settings.gru_size
-        self.big_num = big_num = settings.big_num
-        self.regularizer = settings.regularizer
-        self.lr = settings.lr
         self.session = session
+        self.settings = settings
 
-        self.input_word = tf.placeholder(dtype=tf.int32, shape=[None, num_steps], name='input_word')
-        self.input_pos1 = tf.placeholder(dtype=tf.int32, shape=[None, num_steps], name='input_pos1')
-        self.input_pos2 = tf.placeholder(dtype=tf.int32, shape=[None, num_steps], name='input_pos2')
-        self.input_y = tf.placeholder(dtype=tf.float32, shape=[None, num_classes], name='input_y')
-        self.total_shape = tf.placeholder(dtype=tf.int32, shape=[big_num + 1], name='total_shape')
-        total_num = self.total_shape[-1]
+        self.input_word = tf.placeholder(dtype=tf.int32, shape=[None, self.settings.num_steps], name='input_word')
+        self.input_pos1 = tf.placeholder(dtype=tf.int32, shape=[None, self.settings.num_steps], name='input_pos1')
+        self.input_pos2 = tf.placeholder(dtype=tf.int32, shape=[None, self.settings.num_steps], name='input_pos2')
+        self.input_y = tf.placeholder(dtype=tf.float32, shape=[None, self.settings.num_classes], name='input_y')
+        self.total_shape = tf.placeholder(dtype=tf.int32, shape=[self.settings.big_num + 1], name='total_shape')
+        self.total_num = self.total_shape[-1]
 
-        word_embedding = tf.get_variable(initializer=word_embeddings, name='word_embedding')
-        pos1_embedding = tf.get_variable('pos1_embedding', [settings.pos_num, settings.pos_size])
-        pos2_embedding = tf.get_variable('pos2_embedding', [settings.pos_num, settings.pos_size])
-
-        attention_w = tf.get_variable('attention_omega', [gru_size, 1])
-        sen_a = tf.get_variable('attention_A', [gru_size])
-        sen_r = tf.get_variable('query_r', [gru_size, 1])
-        relation_embedding = tf.get_variable('relation_embedding', [self.num_classes, gru_size])
-        sen_d = tf.get_variable('bias_d', [self.num_classes])
-
-        gru_cell_forward = tf.contrib.rnn.GRUCell(gru_size)
-        gru_cell_backward = tf.contrib.rnn.GRUCell(gru_size)
-
-        if is_training and settings.keep_prob < 1:
-            gru_cell_forward = tf.contrib.rnn.DropoutWrapper(gru_cell_forward, output_keep_prob=settings.keep_prob)
-            gru_cell_backward = tf.contrib.rnn.DropoutWrapper(gru_cell_backward, output_keep_prob=settings.keep_prob)
-
-        cell_forward = tf.contrib.rnn.MultiRNNCell([gru_cell_forward] * settings.num_layers)
-        cell_backward = tf.contrib.rnn.MultiRNNCell([gru_cell_backward] * settings.num_layers)
-
-        sen_repre = []
-        sen_alpha = []
-        sen_s = []
-        sen_out = []
         self.prob = []
         self.predictions = []
         self.loss = []
         self.accuracy = []
         self.total_loss = 0.0
 
-        self._initial_state_forward = cell_forward.zero_state(total_num, tf.float32)
-        self._initial_state_backward = cell_backward.zero_state(total_num, tf.float32)
+        self._initial_state_forward = None
+        self._initial_state_backward = None
 
+        self.word_embeddings = word_embeddings
+        self.is_training = is_training
+
+    def construct_model(self):
+
+        word_embedding = tf.get_variable(initializer=self.word_embeddings, name='word_embedding')
+        pos1_embedding = tf.get_variable('pos1_embedding', [self.settings.pos_num, self.settings.pos_size])
+        pos2_embedding = tf.get_variable('pos2_embedding', [self.settings.pos_num, self.settings.pos_size])
+
+        attention_w = tf.get_variable('attention_omega', [self.settings.hidden_unit, 1])
+        sen_a = tf.get_variable('attention_A', [self.settings.hidden_unit])
+        sen_r = tf.get_variable('query_r', [self.settings.hidden_unit, 1])
+        relation_embedding = tf.get_variable('relation_embedding',
+                                             [self.settings.num_classes, self.settings.hidden_unit])
+        sen_d = tf.get_variable('bias_d', [self.settings.num_classes])
+
+        rnn_cell_forward, rnn_cell_backward = self._bi_dir_rnn()
+
+        cell_forward = tf.contrib.rnn.MultiRNNCell([rnn_cell_forward] * self.settings.num_layers)
+        cell_backward = tf.contrib.rnn.MultiRNNCell([rnn_cell_backward] * self.settings.num_layers)
+
+        sen_repre = []
+        sen_alpha = []
+        sen_s = []
+        sen_out = []
+
+        self._initial_state_forward = cell_forward.zero_state(self.total_num, tf.float32)
+        self._initial_state_backward = cell_backward.zero_state(self.total_num, tf.float32)
 
         # embedding layer
         inputs_forward = tf.concat(axis=2, values=[tf.nn.embedding_lookup(word_embedding, self.input_word),
                                                    tf.nn.embedding_lookup(pos1_embedding, self.input_pos1),
                                                    tf.nn.embedding_lookup(pos2_embedding, self.input_pos2)])
 
-        inputs_backward = tf.concat(axis=2, values=[tf.nn.embedding_lookup(word_embedding, tf.reverse(self.input_word, [1])),
-                                                    tf.nn.embedding_lookup(pos1_embedding, tf.reverse(self.input_pos1, [1])),
-                                                    tf.nn.embedding_lookup(pos2_embedding, tf.reverse(self.input_pos2, [1]))])
+        inputs_backward = tf.concat(axis=2,
+                                    values=[tf.nn.embedding_lookup(word_embedding, tf.reverse(self.input_word, [1])),
+                                            tf.nn.embedding_lookup(pos1_embedding, tf.reverse(self.input_pos1, [1])),
+                                            tf.nn.embedding_lookup(pos2_embedding, tf.reverse(self.input_pos2, [1]))])
 
         outputs_forward = []
 
         state_forward = self._initial_state_forward
 
-        # Bi-GRU layer
-        with tf.variable_scope('GRU_FORWARD') as scope:
-            for step in range(num_steps):
+        with tf.variable_scope('RNN_FORWARD') as scope:
+            for step in range(self.settings.num_steps):
                 if step > 0:
                     scope.reuse_variables()
                 (cell_output_forward, state_forward) = cell_forward.call(inputs_forward[:, step, :], state_forward)
@@ -103,24 +104,29 @@ class GRU:
         outputs_backward = []
 
         state_backward = self._initial_state_backward
-        with tf.variable_scope('GRU_BACKWARD') as scope:
-            for step in range(num_steps):
+        with tf.variable_scope('RNN_BACKWARD') as scope:
+            for step in range(self.settings.num_steps):
                 if step > 0:
                     scope.reuse_variables()
                 (cell_output_backward, state_backward) = cell_backward.call(inputs_backward[:, step, :], state_backward)
                 outputs_backward.append(cell_output_backward)
 
-        output_forward = tf.reshape(tf.concat(axis=1, values=outputs_forward), [total_num, num_steps, gru_size])
-        output_backward = tf.reverse(tf.reshape(tf.concat(axis=1, values=outputs_backward), [total_num, num_steps, gru_size]),[1])
+        output_forward = tf.reshape(tf.concat(axis=1, values=outputs_forward),
+                                    [self.total_num, self.settings.num_steps, self.settings.hidden_unit])
+        output_backward = tf.reverse(tf.reshape(tf.concat(axis=1, values=outputs_backward),
+                                                [self.total_num, self.settings.num_steps, self.settings.hidden_unit]), [1])
 
         # word-level attention layer
         output_h = tf.add(output_forward, output_backward)
         attention_r = tf.reshape(tf.matmul(tf.reshape(tf.nn.softmax(
-            tf.reshape(tf.matmul(tf.reshape(tf.tanh(output_h), [total_num * num_steps, gru_size]), attention_w),
-                       [total_num, num_steps])), [total_num, 1, num_steps]), output_h), [total_num, gru_size])
+            tf.reshape(tf.matmul(
+                tf.reshape(tf.tanh(output_h), [self.total_num * self.settings.num_steps, self.settings.hidden_unit]),
+                attention_w),
+                       [self.total_num, self.settings.num_steps])), [self.total_num, 1, self.settings.num_steps]), output_h),
+            [self.total_num, self.settings.hidden_unit])
 
         # sentence-level attention layer
-        for i in range(big_num):
+        for i in range(self.settings.big_num):
 
             sen_repre.append(tf.tanh(attention_r[self.total_shape[i]:self.total_shape[i + 1]]))
             batch_size = self.total_shape[i + 1] - self.total_shape[i]
@@ -129,8 +135,9 @@ class GRU:
                 tf.reshape(tf.nn.softmax(tf.reshape(tf.matmul(tf.multiply(sen_repre[i], sen_a), sen_r), [batch_size])),
                            [1, batch_size]))
 
-            sen_s.append(tf.reshape(tf.matmul(sen_alpha[i], sen_repre[i]), [gru_size, 1]))
-            sen_out.append(tf.add(tf.reshape(tf.matmul(relation_embedding, sen_s[i]), [self.num_classes]), sen_d))
+            sen_s.append(tf.reshape(tf.matmul(sen_alpha[i], sen_repre[i]), [self.settings.hidden_unit, 1]))
+            sen_out.append(
+                tf.add(tf.reshape(tf.matmul(relation_embedding, sen_s[i]), [self.settings.num_classes]), sen_d))
 
             self.prob.append(tf.nn.softmax(sen_out[i]))
 
@@ -159,8 +166,9 @@ class GRU:
         # tf.summary.scalar('loss',self.total_loss)
         tf.summary.scalar('loss', self.total_loss)
         # regularization
-        self.l2_loss = tf.contrib.layers.apply_regularization(regularizer=tf.contrib.layers.l2_regularizer(self.regularizer),
-                                                              weights_list=tf.trainable_variables())
+        self.l2_loss = tf.contrib.layers.apply_regularization(
+            regularizer=tf.contrib.layers.l2_regularizer(self.settings.regularizer),
+            weights_list=tf.trainable_variables())
         self.final_loss = self.total_loss + self.l2_loss
         tf.summary.scalar('l2_loss', self.l2_loss)
         tf.summary.scalar('final_loss', self.final_loss)
@@ -213,7 +221,7 @@ class GRU:
         if en2pos == -1:
             en2pos = 0
         pos_embedding = []
-        fixlen = self.num_steps
+        fixlen = self.settings.num_steps
         # Encoding test x
         for i in range(fixlen):
             word = word2id['BLANK']
@@ -277,9 +285,37 @@ class GRU:
 
         return batch
 
+    def _witch_cell(self):
+        """
+        RNN 类型
+        :return:
+        """
+        cell_tmp = None
+        if self.settings.cell_type == RNN__CELL_TYPE.LSTM:
+            cell_tmp = tf.contrib.rnn.LSTMCell(self.settings.hidden_unit)
+        elif self.settings.cell_type == RNN__CELL_TYPE.GRU:
+            cell_tmp = tf.contrib.rnn.GRUCell(self.settings.hidden_unit)
+        return cell_tmp
+
+    def _bi_dir_rnn(self):
+        """
+        双向RNN
+        :return:
+        """
+        cell_fw = self._witch_cell()
+        cell_bw = self._witch_cell()
+        if self.is_training and self.settings.keep_prob < 1:
+            cell_bw = tf.contrib.rnn.DropoutWrapper(cell_bw, output_keep_prob=self.settings.keep_prob)
+            cell_fw = tf.contrib.rnn.DropoutWrapper(cell_fw, output_keep_prob=self.settings.keep_prob)
+        return cell_fw, cell_bw
+
 class Batch:
     def __init__(self, word_batch, pos1_batch, pos2_batch, relation_batch):
         self.word_batch = word_batch
         self.pos1_batch = pos1_batch
         self.pos2_batch = pos2_batch
         self.relation_batch = relation_batch
+
+class RNN__CELL_TYPE(Enum):
+      LSTM = 1
+      GRU = 2
